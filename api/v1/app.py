aa3c5014778ae30ecaa62635eaf38bc363ebe7e9
Zeezbaba <Azeez_okeleji>
2024-04-02 00:39:42 -0700

    Updated app file

--- api/v1/app.py
+++ api/v1/app.py
@@ -7,11 +7,13 @@ from models import storage
 from api.v1.views import app_views
 from os import getenv
 from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+cors = CORS(app, resources={r"/api/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
